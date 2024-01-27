0be15b990c29cf89fe4676c17bbe929660313a1f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-01-27 12:56:58 -0300

    feat: add react-router-dom config

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import './global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App'
+import { App } from './app'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
