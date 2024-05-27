727cfa4d947fc3ee790dccf050f326b9595a6c81
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-27 14:46:41 -0300

    feat: add account menu skeletons

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,11 @@ export const api = axios.create({
   baseURL: ENV.VITE_API_URL,
   withCredentials: true,
 })
+
+if (ENV.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
