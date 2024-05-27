102a1ca3b26c678799d9d9aa5bdeebafd68079b2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-27 15:26:32 -0300

    feat: add store profile dialog

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -1,7 +1,18 @@
 import { api } from '@/lib/axios'
 
+type GetManagedRestaurantResponse = {
+  id: string
+  name: string
+  description: string | null
+  managerId: string | null
+  createdAt: string
+  updatedAt: string
+}
+
 export async function getManagedRestaurant() {
-  const response = await api.get('/managed-restaurant')
+  const response = await api.get<GetManagedRestaurantResponse>(
+    '/managed-restaurant',
+  )
 
   return response.data
 }
