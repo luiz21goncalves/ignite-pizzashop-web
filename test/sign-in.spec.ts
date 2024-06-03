5ad18b30adb2db946e35499005ba1b9b8f09d8d2
Luiz <luizhbgoncalves@gmail.com>
2024-06-03 08:23:02 -0300

    test(e2e): sign-in page

---
+++ test/sign-in.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('john.doe@email.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para seu e-mail.',
+  )
+
+  await expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('wrong@email.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Não foi possível enviar o e-mail de autenticação.',
+  )
+
+  await expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(new URL(page.url()).pathname).toEqual('/sign-up')
+})
